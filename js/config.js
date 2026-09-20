aab1951a9dc75e529abddd0e9f7a144e143af1d0
Aditya Jena <adityajena.python@gmail.com>
2026-09-20 11:28:35 +0530

    Configure production API URL

--- js/config.js
+++ js/config.js
@@ -3,5 +3,5 @@ const SUMUKH_CONFIG = {
     window.location.hostname === "localhost" ||
     window.location.hostname === "127.0.0.1"
       ? "http://localhost:3000"
-      : ""
+      : "https://sumukh-production.up.railway.app"
 };
